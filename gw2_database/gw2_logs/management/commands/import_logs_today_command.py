8a3237a5cf7aadf95358096e540cc8341b055511
wiwihere <157903514+wiwihere@users.noreply.github.com>
2024-03-10 13:53:42 +0100

    fix golem still uploading

--- gw2_database/gw2_logs/management/commands/import_logs_today_command.py
+++ gw2_database/gw2_logs/management/commands/import_logs_today_command.py
@@ -69,12 +69,13 @@ class Command(BaseCommand):
                 # Skip upload if log is not in itype_group
                 try:
                     if itype_groups is not None:
-                        boss_name = str(log_path).split("arcdps.cbtlogs")[1].split("/")[1]
+                        boss_name = str(log_path).split("arcdps.cbtlogs")[1].split("\\")[1]
                         if boss_name not in folder_names:
                             print(f"Skipped {log_path}")
                             log_paths_done.append(log_path)
                             continue
-                except IndexError:
+                except IndexError as e:
+                    print("Failed to find bossname, will use log.")
                     pass
 
                 # Upload log
