9c577479495181dadd307ad6590683757aa92c59
wiwihere <157903514+wiwihere@users.noreply.github.com>
2025-01-26 20:49:50 +0100

    rename parse_logs_command
    from import_logs_today_command

--- bin/run_logs_today_fractal.cmd
+++ bin/run_logs_today_fractal.cmd
@@ -2,5 +2,5 @@
 REM example today: run_logs
 REM example specific day: run_logs 2023 1 29
 set itype_groups=fractal
-call  %~dp0utilities/django.cmd import_logs_today_command --itype_groups %itype_groups%
+call  %~dp0utilities/django.cmd parse_logs_command --itype_groups %itype_groups%
 pause
