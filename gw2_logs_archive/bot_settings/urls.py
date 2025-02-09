17c9a903c4d5a4d6877c193dbfa9d0554add92f5
wiwihere <157903514+wiwihere@users.noreply.github.com>
2025-02-09 10:03:54 +0100

    add favicon

--- gw2_logs_archive/bot_settings/urls.py
+++ gw2_logs_archive/bot_settings/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
 # urlpatterns = [
 #     # '',
