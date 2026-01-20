8a51468760acd4f1421c6269bb1defa0e70ac5f5
wiwihere <157903514+wiwihere@users.noreply.github.com>
2026-01-20 20:32:38 +0100

    add heatmap

--- gw2_logs_archive/scripts/ei_parser.py
+++ gw2_logs_archive/scripts/ei_parser.py
@@ -23,7 +23,7 @@ EI_SETTINGS_DEFAULT = settings.BASE_DIR.joinpath("bot_settings", "gw2ei_settings
 
 class EliteInsightsParser:
     def __init__(self):
-        """Interaction with EliteInisghts CLI"""
+        """Interaction with EliteInsights CLI"""
         self.EI_exe = EI_PARSER_FOLDER.joinpath("GuildWars2EliteInsights-CLI.exe")
         self.out_dir = None  # Set in .create_settings
         self.settings = None  # Set in .create_settings
@@ -31,38 +31,45 @@ class EliteInsightsParser:
         self.download_or_update_EI()
 
     def download_or_update_EI(self):
-        """Download or update EliteInisghts CLI
+        """Download or update EliteInsights CLI
 
-        Automatically checks version every week.
+        Automatically checks version only once every week.
         """
         call_command("update_elite_insights_version", auto_update_check=True)
 
-    def create_settings(self, out_dir, setting_in_path=EI_SETTINGS_DEFAULT, create_html=False):
+    def create_settings(
+        self,
+        out_dir: Path,
+        setting_in_path: Path = EI_SETTINGS_DEFAULT,
+        create_html: bool = False,
+    ) -> None:
         """
-        ei_settings: str
+        Write conf file to settings_out_path.
+
+        setting_in_path : str
             Path to ei settings to use. Will be edited based on inputs
-        out_dir: str
+        out_dir : str
             Directory where output jsons are placed.
-        create_html: bool, default False
+        create_html : bool, default False
             Also create html of the fight
         """
 
         self.out_dir = out_dir
         out_dir.mkdir(exist_ok=True)
 
-        setting_out_path = out_dir.joinpath("gw2ei_settings.conf")
+        setting_output_path = out_dir.joinpath("gw2ei_settings.conf")
 
         # Load default settings
-        set_default = setting_in_path.read_text()
+        settings_default = setting_in_path.read_text()
 
         # Format the str with provided variables
-        set_str = set_default.format(create_html=create_html, out_dir=out_dir)
+        settings_output = settings_default.format(create_html=create_html, out_dir=out_dir)
 
         # Create settigns in output folder
-        setting_out_path.write_text(set_str)
-        self.settings = setting_out_path
+        setting_output_path.write_text(settings_output)
+        self.settings = setting_output_path
 
-    def parse_log(self, evtc_path) -> Path:
+    def parse_log(self, evtc_path: Path) -> Path:
         """Parse to json locally. Uploading to dps.report is not implemented.
         returns evtc_path=None when process doesnt parse the log. For instance due to
         Program: Fight is too short: 0 < 2200
@@ -79,18 +86,18 @@ class EliteInsightsParser:
         if self.settings is None:
             raise ValueError("Run self.create_settings first.")
 
-        js_path = self.find_parsed_json(evtc_path=evtc_path)
+        js_path = self._find_parsed_json(evtc_path=evtc_path)
 
         if js_path:
             logger.info(f"Log {evtc_path.name} already parsed")
         else:
             # Call the parser
             res = subprocess.run([str(self.EI_exe), "-c", f"{self.settings}", evtc_path])
-            js_path = self.find_parsed_json(evtc_path=evtc_path)
+            js_path = self._find_parsed_json(evtc_path=evtc_path)
 
         return js_path
 
-    def find_parsed_json(self, evtc_path):
+    def _find_parsed_json(self, evtc_path: Path) -> Path:
         """Output gets a bit of a different name, find it."""
         evtc_path = Path(evtc_path)
 
@@ -115,10 +122,10 @@ class EliteInsightsParser:
 
 if __name__ == "__main__":
     self = ei_parser = EliteInsightsParser()
-    out_dir = settings.EI_PARSED_LOGS_DIR.joinpath("20250125")
+    out_dir = settings.EI_PARSED_LOGS_DIR.joinpath("202512081")
     setting_in_path = EI_SETTINGS_DEFAULT
-    create_html = False
-    ei_parser.create_settings(out_dir=out_dir, setting_in_path=setting_in_path, create_html=False)
+    create_html = True
+    ei_parser.create_settings(out_dir=out_dir, setting_in_path=setting_in_path, create_html=create_html)
 
     d = ei_parser.parse_log(evtc_path=r"")
     r2 = EliteInsightsParser.load_json_gz(js_path=d)
