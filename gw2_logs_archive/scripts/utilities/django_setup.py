92690e0dd84073c76f4c1f81a4f466c7617884cf
wiwihere <157903514+wiwihere@users.noreply.github.com>
2026-01-24 13:15:54 +0100

    Cleanup .env

--- gw2_logs_archive/scripts/utilities/django_setup.py
+++ gw2_logs_archive/scripts/utilities/django_setup.py
@@ -4,5 +4,8 @@ import django
 
 
 def run():
+    """Together with PYTHONPATH and DJANGO_SETTINGS_MODULE in the .env,
+    this  will setup django to work within jupyter interactive windows.
+    """
     os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
     django.setup()
