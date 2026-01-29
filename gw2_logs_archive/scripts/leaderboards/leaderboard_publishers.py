6dd8c027fdc2329e669a7b5fd6ced5e34f66e1cf
wiwihere <157903514+wiwihere@users.noreply.github.com>
2026-01-29 23:55:01 +0100

    create if not exists

--- gw2_logs_archive/scripts/leaderboards/leaderboard_publishers.py
+++ gw2_logs_archive/scripts/leaderboards/leaderboard_publishers.py
@@ -93,7 +93,7 @@ def publish_navigation_menu() -> None:
         embeds_messages_list.append(create_navigation_embed(instance_type, leaderboard_thread_url))
 
     discord_message_name = "leaderboard_navigation"
-    discord_message = DiscordMessage.objects.get(name=discord_message_name)
+    discord_message = DiscordMessage.objects.get_or_create(name=discord_message_name)
 
     _ = send_discord_message(
         discord_message=discord_message,
